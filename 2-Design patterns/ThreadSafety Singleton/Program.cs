fc5920eb58bb04abdaf6b7a42473ec9d1f8fb57c
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-03-07 23:32:35 +0200

    SingleTon Thread Safety version

---
+++ 2-Design patterns/ThreadSafety Singleton/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ThreadSafety_Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            SingleTon singleTon1 = SingleTon.GetInstance();
+
+            SingleTon singleTon2 = SingleTon.GetInstance();
+
+            if(singleTon1 == singleTon2) Console.WriteLine("ST verified");
+
+            SingleTon singleTon3 = SingleTon.GetInstance();
+
+            if (singleTon3 == singleTon2) Console.WriteLine("ST verified");
+
+            SingleTon singleTon4, singleTon5;
+
+
+            Thread t1 = new Thread(() =>
+            {
+                SingleTon singleTon = SingleTon.GetInstance();
+
+                singleTon4 = singleTon;
+
+
+                if (singleTon4 == singleTon2) Console.WriteLine("ST verified");
+            });
+
+            Thread t2 = new Thread(() =>
+            {
+                SingleTon singleTon = SingleTon.GetInstance();
+
+                singleTon5 = singleTon;
+
+                if (singleTon3 == singleTon5) Console.WriteLine("ST verified");
+            });
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+            Console.WriteLine(SingleTon.noOfInstances);
+
+
+        }
+    }
+}
---
+++ 2-Design patterns/ThreadSafety Singleton/SingleTon.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadSafety_Singleton
+{
+    internal class SingleTon
+    {
+
+        public static int noOfInstances = 0;
+        private static  SingleTon instance = null;
+
+        private static readonly object Lock = new object();
+        private SingleTon() { }
+
+
+        
+        public static SingleTon GetInstance()
+        {
+            if (instance != null) return instance;
+
+            lock (Lock)
+            {
+                if (instance == null)
+                {
+                    Console.WriteLine("Creating Instance for 1st time");
+
+                    instance = new SingleTon();
+                    noOfInstances++;
+
+                    return instance;
+                }
+
+                return instance;
+            }
+            
+        }
+
+
+    }
+}
